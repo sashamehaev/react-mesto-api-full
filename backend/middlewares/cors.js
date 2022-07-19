edb3ba23fcac1fcbcabd9c59ed854d5c517ff7ab
Alexandr Mekhaev <alexandrmekhaev@yandex.ru>
2022-07-20 00:36:14 +0300

    project 15

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -1,11 +1,7 @@
 const allowedCors = [
-  'https://api.sashamehaev-mesto.nomoredomains.xyz',
-  'http://api.sashamehaev-mesto.nomoredomains.xyz',
-  'api.sashamehaev-mesto.nomoredomains.xyz',
-  'https://sashamehaev-mesto.nomoredomains.xyz',
-  'http://sashamehaev-mesto.nomoredomains.xyz',
-  'sashamehaev-mesto.nomoredomains.xyz',
-  'localhost:3000'
+  'https://praktikum.tk',
+  'http://praktikum.tk',
+  'localhost:3000',
 ];
 
 module.exports = (req, res, next) => {
@@ -15,11 +11,12 @@ module.exports = (req, res, next) => {
     res.header('Access-Control-Allow-Origin', origin);
   }
 
-  res.header('Access-Control-Allow-Origin', "*");
+  res.header('Access-Control-Allow-Origin', '*');
 
   const { method } = req;
 
-  const DEFAULT_ALLOWED_METHODS = "GET,HEAD,PUT,PATCH,POST,DELETE";
+  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+  const requestHeaders = req.headers['access-control-request-headers'];
 
   if (method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
